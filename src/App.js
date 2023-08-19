36154131dd130474e50372047f7c2524a763aeb4
Thi-Ponce <thi.pponce@gmail.com>
2023-08-19 17:22:24 -0300

    integrate state and searchbox for cardlist

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-/* eslint-disable react/prefer-stateless-function */
+/* eslint-disable */
 import React, { Component } from 'react';
 import './App.css';
 import CardList from './components/CardList';
+import SearchBox from './components/SearchBox';
 import { robots } from './robots';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchfield: '',
+      };
+    }
+
+    onSearchChange = (event) => {
+      this.setState({ searchfield: event.target.value })
+    };
+
   render() {
+    const filteredRobots = this.state.robots.filter(robots => {
+      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+      })
     return (
-      <CardList robots={robots} />
+      <div className="tc">
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
+      </div>
     );
   }
 }
---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable arrow-body-style */
+import React from 'react';
+
+const SearchBox = ({ searchChange }) => {
+  return (
+    <div className="pa2">
+      <input
+        className="pa3 ba b--green bg-lightest-blue"
+        type="search"
+        placeholder="search robots"
+        onChange={searchChange}
+      />
+    </div>
+  );
+};
+
+export default SearchBox;
