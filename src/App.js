ea11c8448b542281e2153d8b2e4e0f32d8b17630
Thi-Ponce <thi.pponce@gmail.com>
2023-08-19 16:32:03 -0300

    upgrade cards with props

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import Card from './components/Card';
+import { robots } from './robots';
 
 class App extends Component {
   render() {
     return (
-      <Card />
+      <div>
+        <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+        <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
+        <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+        <Card id={robots[3].id} name={robots[3].name} email={robots[3].email} />
+      </div>
     );
   }
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,21 @@
+/* eslint-disable react/jsx-curly-brace-presence */
+/* eslint-disable no-template-curly-in-string */
+/* eslint-disable react/prop-types */
 import React from 'react';
 
-const Card = () => (
-  <div className="card bg-light-green dib br3 pa3 ma2 grow">
-    <img src="https://robohash.org/test?200x200" alt="robots" />
+const Card = ({ name, email, id }) => (
+  <div className="card bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+    <img
+      src={`https://robohash.org/${id}?200x200`}
+      alt="robots"
+    />
     <div className="card-body">
-      <h2 className="card-title">Random Image</h2>
-      <p className="card-text">Random Image Generator</p>
+      <h2 className="card-title">
+        {name}
+      </h2>
+      <p className="card-text">
+        {email}
+      </p>
     </div>
   </div>
 );
