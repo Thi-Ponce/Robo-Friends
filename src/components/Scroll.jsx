cfbe60725b7bd8070b02e2146528151ba7040f82
Thi-Ponce <thi.pponce@gmail.com>
2023-08-19 18:10:58 -0300

    add scroll container

---
+++ src/components/Scroll.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+const Scroll = ({ children }) => (
+  <div style={{ overflowY: 'scroll', border: '1px solid black', height: '500px' }}>
+    {children}
+  </div>
+);
+
+export default Scroll;
