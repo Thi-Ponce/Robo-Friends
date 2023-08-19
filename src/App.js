6c89c560cc77fba911c02f2fd57bddd45dfc806c
Thi-Ponce <thi.pponce@gmail.com>
2023-08-19 16:54:49 -0300

    use map for array of cards, create card list

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
 import './App.css';
-import Card from './components/Card';
+import CardList from './components/CardList';
 import { robots } from './robots';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-        <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-        <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
-        <Card id={robots[3].id} name={robots[3].name} email={robots[3].email} />
-      </div>
+      <CardList robots={robots} />
     );
   }
 }
